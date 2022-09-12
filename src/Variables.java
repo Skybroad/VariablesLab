bf5bce37acb873152ef92b2b2700bdbd226b1d7a
Skybroad <tianbozhangny@gmail.com>
2022-09-12 09:53:27 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,29 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short l = -234;
+        int f = 423443;
+        long b = 432479092L;
+
+        float boat = 34.234F;
+        double triple = 423425234.2342343523;
+        char something = 'L';
+        boolean WyattBrumleytakesLs = true;
+        
+        System.out.println(f + l);
+        System.out.println(b + l);
+        System.out.println(boat + triple);
+        System.out.println(something);
+        System.out.println(byteOne);
+        System.out.println(WyattBrumleytakesLs);
+    }
+}
